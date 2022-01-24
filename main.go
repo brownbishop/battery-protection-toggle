3f0e25ca3d2c21552c0e2fdb01af2b1a1b8cfc5d
brownbishop <catalin1second@gmail.com>
2022-01-24 11:26:54 +0200

    fix status function and improve message

--- main.go
+++ main.go
@@ -9,48 +9,53 @@ import (
 const LOCATION = "/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode"
 
 func usage() {
-    message := `
-    usage: battery-protection-toggle [operation]
+	message := `usage: battery-protection-toggle [operation]
     operations:
         -h  Display this message.
         -s  Display battery protection status.
         -e  Enable battery protection.
         -d  Disable battery protection.
     `
-    fmt.Printf(message)
+	fmt.Printf(message)
 }
 
 func status() {
-    status, err := os.ReadFile(LOCATION)
+	file, err := os.Open(LOCATION)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	var status int
+	fmt.Fscanf(file, "%d", &status)
 
-
-    switch string(status[:]) {
-    case "1":
-        fmt.Println("battery protection enabled")
-    case "0":
-        fmt.Println("battery protection disabled")
-    }
+	switch status {
+	case 1:
+		fmt.Println("battery protection enabled")
+	case 0:
+		fmt.Println("battery protection disabled")
+	}
 }
 
 func enable_protection() {
-    err := os.WriteFile(LOCATION, []byte("1"), 0644)
-    if err != nil {
-        log.Fatal(err)
-    }
+	err := os.WriteFile(LOCATION, []byte("1"), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func disable_protection() {
-    err := os.WriteFile(LOCATION, []byte("0"), 0644)
-    if err != nil {
-        log.Fatal(err)
-    }
+	err := os.WriteFile(LOCATION, []byte("0"), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
+	if len(os.Args) != 2 {
+		usage()
+		os.Exit(0)
+	}
+
 	switch os.Args[1] {
 	case "-s":
 		status()
@@ -58,8 +63,8 @@ func main() {
 		enable_protection()
 	case "-d":
 		disable_protection()
-    case "-h":
-        usage()
+	case "-h":
+		usage()
 	default:
 		log.Print("unknown option")
 	}
