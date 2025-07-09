172829bc170d95f74097e9140ddd954bc6f3a20a
brownbishop <catalin1second@gmail.com>
2025-07-09 19:22:02 +0300

    use cli framework for saner interface

--- main.go
+++ main.go
@@ -1,25 +1,30 @@
 package main
 
 import (
-	"flag"
+	"context"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"strings"
+
+	"github.com/urfave/cli/v3"
 )
 
 const LOCATION = "/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode"
 
-func displayStatus() {
+func displayStatus() error {
 	file, err := os.Open(LOCATION)
 	defer file.Close()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	var status int
-	fmt.Fscanf(file, "%d", &status)
+	_, err = fmt.Fscanf(file, "%d", &status)
+	if err != nil {
+		return err
+	}
 
 	switch status {
 	case 1:
@@ -27,62 +32,81 @@ func displayStatus() {
 	case 0:
 		fmt.Println("battery protection disabled")
 	}
+	return nil
 }
 
-func enableProtection() {
+func enableProtection() error {
 	err := os.WriteFile(LOCATION, []byte("1"), 0644)
 	if err != nil {
 		if strings.Contains(err.Error(), "permission denied") {
-			runAsRoot("-on")
+			return runAsRoot("-e")
 		} else {
-			log.Fatal(err)
+			return err
 		}
 	}
+
+	return nil
 }
 
-func disableProtection() {
+func disableProtection() error {
 	err := os.WriteFile(LOCATION, []byte("0"), 0644)
 	if err != nil {
 		if strings.Contains(err.Error(), "permission denied") {
-			runAsRoot("-off")
+			return runAsRoot("-d")
 		} else {
-			log.Fatal(err)
+			return err
 		}
 	}
+	return nil
 }
 
-func runAsRoot(arg string) {
+func runAsRoot(arg string) error {
 	fmt.Println("This operation requires root privileges, running again with sudo...")
 	exe, err := os.Executable()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	cmd := exec.Command("sudo", exe, arg)
 	err = cmd.Run()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+	return nil
+}
+
+func action(ctx context.Context, cmd *cli.Command) error {
+	var err error
+
+	if cmd.Bool("enable") {
+		err = enableProtection()
+	}
+
+	if cmd.Bool("disable") {
+		err = disableProtection()
+	}
+
+	if cmd.Bool("status") {
+		err = displayStatus()
+	}
+
+	return err
 }
 
 func main() {
-	status := flag.Bool("status", false, "battery protection status")
-	on := flag.Bool("on", false, "turn on battery protection")
-	off := flag.Bool("off", false, "turn off battery protection")
-	help := flag.Bool("help", false, "display help message")
+	cmd := &cli.Command{
+		UseShortOptionHandling: true,
+		EnableShellCompletion: true,
+		Name:                   "battery-protection-toggle",
+		Usage:                  "Control battery protection mode on Lenovo laptops.",
+		Flags: []cli.Flag{
+			&cli.BoolFlag{Name: "enable", Aliases: []string{"e"}},
+			&cli.BoolFlag{Name: "disable", Aliases: []string{"d"}},
+			&cli.BoolFlag{Name: "status", Aliases: []string{"s"}, Value: true},
+		},
+		Action: action,
+	}
 
-	flag.Parse()
-
-	switch {
-	case *status == true:
-		displayStatus()
-	case *on == true:
-		enableProtection()
-	case *off == true:
-		disableProtection()
-	case *help == true:
-		flag.Usage()
-	default:
-		flag.Usage()
-		os.Exit(1)
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
 	}
 }
